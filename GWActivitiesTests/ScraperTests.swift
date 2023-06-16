d6f6bc62f8bd78a634bc84bc89104c796d7adbf2
Ludovic HENRY <badsteam@hotmail.com>
2023-06-15 22:17:23 -0400

    finished unit tests on the Scraper

--- GWActivitiesTests/ScraperTests.swift
+++ GWActivitiesTests/ScraperTests.swift
@@ -14,39 +14,79 @@ class NetworkingMock: Networking {
     func data(from url: URL, delegate: URLSessionTaskDelegate?) async throws -> (Data, URLResponse) {
         try (result.get(), URLResponse())
     }
+}
 
+enum FakeData {
+    case dailyOK, dailyKO, weklyOK, weeklyKO
 }
 
 final class ScraperTests: XCTestCase {
-
-    private var scraper: Scraper!
     private var networking: NetworkingMock!
-    private var fakeWebPage: Data!
 
     override func setUp() {
         super.setUp()
         networking = NetworkingMock()
-        let fileName = "fakeDay"
+    }
 
-        let path = Bundle.main.path(forResource: fileName, ofType: "html")
+// MARK: Test Scraping Day Activity
+
+    func testScrapGoodDayActivityWebPage() async throws {
+        networking.result = .success(loadFile(.dailyOK))
+        let data: [DayActivity] = try await Scraper.getActivities(DayActivity.self, networking: networking)
+        XCTAssertEqual(data.count, 73)
+        XCTAssertEqual(data[5].nicholas_sandford.title, "Spider Legs")
+    }
+
+    func testScrapBadDayActivityWebPage() async throws {
+        networking.result = .success(loadFile(.dailyKO))
+        do {
+            _ = try await Scraper.getActivities(DayActivity.self, networking: networking)
+        } catch {
+            XCTAssertEqual(ScraperError.failedExtractingData, error as! ScraperError)
+        }
+    }
+
+// MARK: Test Scraping Day Activity
+
+    func testScrapGoodWeekActivityWebPage() async throws {
+        networking.result = .success(loadFile(.weklyOK))
+        let data: [WeekActivity] = try await Scraper.getActivities(WeekActivity.self, networking: networking)
+        XCTAssertEqual(data.count, 12)
+        XCTAssertEqual(data[2].nicholas_location.title, "Barbarous Shore")
+    }
+
+    func testScrapBadWeekActivityWebPage() async throws {
+        networking.result = .success(loadFile(.weeklyKO))
+        do {
+            _ = try await Scraper.getActivities(DayActivity.self, networking: networking)
+        } catch {
+            XCTAssertEqual(ScraperError.failedExtractingData, error as! ScraperError)
+        }
+    }
+}
+
+extension ScraperTests {
+
+    private func loadFile(_ fakeType: FakeData) -> Data {
+        var result = Data()
+        var fileName = ""
+        switch fakeType {
+        case .dailyOK:
+            fileName = "FakeDayOK"
+        case .dailyKO:
+            return result
+        case .weklyOK:
+            fileName = "FakeWeekOK"
+        case .weeklyKO:
+            return result
+        }
+        let bundleDoingTest = Bundle(for: type(of: self ))
+        let path = bundleDoingTest.path(forResource: fileName, ofType: "html")
             do {
-                fakeWebPage = try Data(contentsOf: URL(filePath: path!))
+                result = try Data(contentsOf: URL(filePath: path!))
             } catch {
                 print("nil")
             }
-    }
-
-    func testLoadDayActivity() async throws {
-        networking.result = .success(fakeWebPage)
-//        let aa = try await Scraper.getActivities(DayActivity.self, networking: networking)
-//        let data: [DayActivity] = try await Scraper.getActivities(DayActivity.self)
-        let data: [DayActivity] = try await Scraper.getActivities(DayActivity.self, networking: networking)
-        XCTAssertEqual(data.count, 73)
-        
-    }
-
-    func testLoadWeekActivity1() async throws {
-        let data: [WeekActivity] = try await Scraper.getActivities(WeekActivity.self)
-        XCTAssertNotEqual(data.count, 0)
+        return result
     }
 }
